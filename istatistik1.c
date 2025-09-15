9e54e5ec9c966c45c9534deff30abd9bd9bbec2b
Yusuf Mert Özdemir <yusufmert1358@gmail.com>
2025-09-15 09:08:37 +0300

    degişiklik

---
+++ istatistik1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "istatistik1.h"
+#include "istatistik2.h"
+
+static int is_func_valid(char op);
+
+// Ana istatiksel fonksiyonu
+void istatik(double *dizi, int boyut, char op)
+{
+    if (is_func_valid(op))
+    {
+        internal_istatiksel(dizi, boyut, op);
+    }
+    else
+    {
+        printf("Gecersiz islem: %c\n", op);
+    }
+}
+int is_func_valid(char op)
+{
+    switch (op)
+    {
+    case 'o':
+    case 'v':
+    case 's':
+        return 1;
+    default:
+        return 0;
+    }
+}
